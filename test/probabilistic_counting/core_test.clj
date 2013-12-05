4cfc829e3386683789ae82cd4a344f69e167f0c4
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-04 20:33:03 -0500

    initial commit

---
+++ test/probabilistic_counting/core_test.clj
@@ -0,0 +1,7 @@
+(ns probabilistic-counting.core-test
+  (:require [clojure.test :refer :all]
+            [probabilistic-counting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

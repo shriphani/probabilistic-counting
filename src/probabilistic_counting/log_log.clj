055485650084aa9589f92d8050d557c3bdce8c7d
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-04 23:06:34 -0500

    current state has a bug in it

--- src/probabilistic_counting/log_log.clj
+++ src/probabilistic_counting/log_log.clj
@@ -1,5 +1,6 @@
 (ns probabilistic-counting.log-log
-  "The LogLog algorithnm")
+  "The LogLog algorithnm"
+  (:use [incanter.core]))
 
 (defn rho
   "Number of leading zeros in the bit-representation
@@ -17,6 +18,29 @@
              (Math/log 2)))))))
 
 (defn log-log
-  [M num-buckets]
-  (let [buckets (map short (range num-buckets))]
-    buckets))
+  [stream num-buckets-exponent]
+  (let [buckets  (make-array Integer/TYPE (Math/pow 2 num-buckets-exponent))
+        j-mask   (bit-shift-left
+                  (- (int (Math/pow 2 num-buckets-exponent)) 1)
+                  (- 32 num-buckets-exponent))
+        val-mask (bit-xor j-mask 0xffff)
+        m        (Math/pow 2 num-buckets-exponent)
+        alpha    (Math/pow
+                  (* (gamma (- (/ 1 m)))
+                     (/ (- 1 (Math/pow 2 (/ 1 m)))
+                        (Math/log 2)))
+                  (- m))]
+    (do
+      (doseq [x stream]
+        (let [index (bit-and x j-mask)
+              val   (bit-and x val-mask)]
+          (println :x (Integer/toBinaryString x)
+                   :index (Integer/toBinaryString index)
+                   :val (Integer/toBinaryString (rho val)))
+          (aset buckets index (max (aget buckets index) (rho val)))))
+      (*
+       0.79402
+       m
+       (Math/pow
+        2 (/ (apply + buckets)
+             (count buckets)))))))

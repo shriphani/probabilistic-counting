4a885af607eaa809687d683e18525eb8f15c865e
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-07 15:03:11 -0500

    updated demo to count a million distinct items

--- src/probabilistic_counting/demo.clj
+++ src/probabilistic_counting/demo.clj
@@ -3,6 +3,5 @@
   (:use [probabilistic-counting.log-log]))
 
 (defn demo-log-log []
-  (map
-   (fn [_] (log-log (map #(mod % 12) (range 100000)) 2))
-   (range 10)))
+  (log-log
+   (map #(mod % 1000000) (range 10000000)) 10))

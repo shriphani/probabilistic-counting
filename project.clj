1a6e6596bf1dd6dcff021198cf94c89f9f4bc94a
shriphani <spalakod@cs.cmu.edu>
2013-12-13 22:09:24 -0500

    exploding fish added

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.mahout/mahout-math "0.8"]
                  [org.apache.commons/commons-lang3 "3.1"]
+                 [org.bovinegenius/exploding-fish "0.3.4"]
                  [incanter/incanter-core "1.5.4"]])
